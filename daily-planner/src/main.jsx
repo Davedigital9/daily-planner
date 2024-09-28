6e005f56aa6115115e38911964a536e1e12441e4
David <151574925+Davedigital9@users.noreply.github.com>
2024-09-28 09:59:07 +0100

    Updated the app name and the default tasks required for completion

--- daily-planner/src/main.jsx
+++ daily-planner/src/main.jsx
@@ -5,9 +5,9 @@ import './index.css'
 
 // eslint-disable-next-line react-refresh/only-export-components
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false },
+  { id: "todo-0", name: "Meditate", completed: false },
+  { id: "todo-1", name: "Exercise", completed: false },
+  { id: "todo-2", name: "Review yesterdays tasks", completed: false },
 ];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
