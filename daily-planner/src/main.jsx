60b64686258af775ba309057d5583fe46217af24
David <151574925+Davedigital9@users.noreply.github.com>
2024-09-28 09:49:31 +0100

    Effected the functionality in the code and allowed for addition of tasks and deleting. Added classisfication of tasks between all, active and completed.

--- daily-planner/src/main.jsx
+++ daily-planner/src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// eslint-disable-next-line react-refresh/only-export-components
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
   { id: "todo-1", name: "Sleep", completed: false },
   { id: "todo-2", name: "Repeat", completed: false },
 ];
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App tasks={DATA} />
-  </StrictMode>,
+  </React.StrictMode>,
 )
